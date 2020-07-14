539bb906200719baaa36d50eee9dc37315119573
Aubrey Uriel Sijo-Gonzales <ASijoGonzales@merchantsbonding.com>
2020-07-14 10:07:49 -0500

    Finished index, show, new, create, edit, update article actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,44 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # Display the parameters for the article coming in from the form
+    #render plain: params[:article].inspect
+
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private 
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end  
 end
