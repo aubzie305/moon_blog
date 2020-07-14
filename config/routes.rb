a312e8cdefa34df89804b05cd8510f8fa0b03777
Aubrey Uriel Sijo-Gonzales <ASijoGonzales@merchantsbonding.com>
2020-07-14 10:30:09 -0500

    Finished destroy articles action

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get '/articles', to: 'articles#index'
   root 'articles#index'
-  get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new', as: :new_article
-  get '/articles/:id', to: 'articles#show', as: :article
-  post '/articles', to: 'articles#create'
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch '/articles/:id', to: 'articles#update'
+  #get '/articles', to: 'articles#index'
+  #get '/articles/new', to: 'articles#new', as: :new_article
+  #get '/articles/:id', to: 'articles#show', as: :article
+  #post '/articles', to: 'articles#create'
+  #get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  #patch '/articles/:id', to: 'articles#update'
+  #delete '/articles/:id', to: 'articles#destroy'
+  # Lines 5-11 can be replaced with the line of code below
+  resource :articles
 end
