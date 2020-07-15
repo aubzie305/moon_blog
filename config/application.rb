eca3736422d24a4e213ba4ba4763c889e68a30a4
Aubrey Uriel Sijo-Gonzales <ASijoGonzales@merchantsbonding.com>
2020-07-15 08:46:56 -0500

    Compiled Assets

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module MoonBlog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.precompile = %w( *.js *.css )
   end
 end
