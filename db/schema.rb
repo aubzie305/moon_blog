53f880200dfeb490fae77959039c84fe3e8444bf
Aubrey Uriel Sijo Gonzales <ASijoGonzales@merchantsbonding.com>
2020-07-27 08:38:14 -0500

    Rollback on User/Article association migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_133729) do
+ActiveRecord::Schema.define(version: 2020_07_16_154329) do
 
   create_table "articles", force: :cascade do |t|
+    t.integer "user_id"
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
     t.string "commenter"
     t.text "body"
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +46,5 @@ ActiveRecord::Schema.define(version: 2020_07_20_133729) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
